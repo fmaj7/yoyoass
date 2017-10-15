15beef7a07eadafa7fd7200a865d15400f169d37
fmaj7 <dingguanghao@gmail.com>
2017-10-15 16:08:13 -0700

    Switch to python 3.6 and return fixed audio in Lambda

--- index.py
+++ index.py
@@ -1,12 +1,42 @@
 import json
 import datetime
+import boto3
+from contextlib import closing
+import base64
 
+CHUNK_SIZE = 1024
+client = boto3.client('polly')
 
 def handler(event, context):
     data = {
         'output': 'Hello World, yoyoasses',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+
+    #TODO: figure out how to use application/x-json-stream
+    response = client.synthesize_speech(
+        OutputFormat='mp3',
+        Text='Welcome to space needle!',
+        VoiceId='Emma'
+    )
+    print("polly response")
+    print(response)
+
+    audio = None
+    if "AudioStream" in response:
+        with closing(response.get("AudioStream")) as auto_closing_audio_stream:
+            audio = auto_closing_audio_stream.read()
+
+    base64_audio = base64.b64encode(audio).decode('ascii')
+
+    return {
+        'statusCode': 200,
+        'body': {
+            "audio": {
+                "data": base64_audio,
+                "format": "mp3",
+                "encoding": "base64"
+            }
+        },
+        'headers': {'Content-Type': 'application/json'}
+    }
